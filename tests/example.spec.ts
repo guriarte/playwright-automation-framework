96e9ad9bd7a4575b7b4eff6c4c94539cdb5de5f0
German Uriarte <uriarte.german@gmail.com>
2024-10-18 14:23:13 -0400

    First test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,71 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('Customer logs in, searches and purchases a product', async ({ page }) => {
+  const responsePromise = page.waitForResponse(
+    'http://localhost:8091/users/login',
+  );
+  await page.goto('http://localhost:4200/');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+  await page.locator('[data-test="nav-sign-in"]').click();
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  expect(page.waitForURL('**/auth/login'));
+  await page
+    .locator('[data-test="email"]')
+    .fill('customer@practicesoftwaretesting.com');
+  await page.locator('[data-test="password"]').fill('welcome01');
+  await page.locator('[data-test="login-submit"]').click();
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  const response = await responsePromise;
+  expect(response.status()).toBe(200);
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await expect(page.locator('[data-test="nav-profile"]')).toBeVisible();
+  expect(page.waitForURL('**/account'));
+
+  await page.getByRole('link', { name: 'Practice Software Testing -' }).click();
+  await page.locator('[data-test="search-query"]').fill('Combination Pliers');
+  await page.keyboard.press('Enter');
+  await page.getByAltText('Combination Pliers').click();
+  expect(page.url()).toContain('/product');
+  await page
+    .locator('[data-test="increase-quantity"]')
+    .click({ clickCount: 2 });
+  expect(page.locator('[data-test="quantity"]')).toHaveValue('3');
+
+  await page.locator('[data-test="add-to-cart"]').click();
+  await expect(
+    page.locator('div').filter({ hasText: 'Product added to shopping' }).nth(2),
+  ).toBeVisible();
+
+  await page.locator('[data-test="nav-cart"]').click();
+  await expect(page.locator('[data-test="proceed-1"]')).toBeVisible();
+  expect(page.url()).toContain('/checkout');
+  await page.getByLabel('Quantity for Combination').clear();
+  await page.getByLabel('Quantity for Combination').fill('10');
+  await page.locator('[data-test="proceed-1"]').click();
+  await expect(page.getByText('You can proceed to checkout')).toBeVisible();
+  await page.locator('[data-test="proceed-2"]').click();
+  await page.locator('[data-test="address"]').clear();
+  await page.locator('[data-test="address"]').fill('123 Fake St');
+  await page.locator('[data-test="city"]').clear();
+  await page.locator('[data-test="city"]').fill('Fake City');
+  await page.locator('[data-test="state"]').clear();
+  await page.locator('[data-test="state"]').fill('Fake State');
+  await page.locator('[data-test="country"]').clear();
+  await page.locator('[data-test="country"]').fill('Fake Country');
+  await page.locator('[data-test="postcode"]').clear();
+  await page.locator('[data-test="postcode"]').fill('12345');
+  await page.locator('[data-test="proceed-3"]').click();
+  await page
+    .locator('[data-test="payment-method"]')
+    .selectOption('Credit Card');
+  await page
+    .locator('[data-test="credit_card_number"]')
+    .fill('1234-5678-1234-5678');
+  await page.locator('[data-test="expiration_date"]').fill('11/2040');
+  await page.locator('[data-test="cvv"]').fill('123');
+  await page.locator('[data-test="card_holder_name"]').fill('Jane Doe');
+  await page.locator('[data-test="finish"]').click();
+  await expect(page.getByText('Payment was successful')).toBeVisible();
+  await page.locator('[data-test="finish"]').click();
+  await expect(page.getByText('Thanks for your order!')).toBeVisible();
 });
